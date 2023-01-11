a857da7335f557daf73fe1a06a1689f894b1e710
Amaar Hassnain <93473921+Amaar015@users.noreply.github.com>
2023-01-11 12:19:16 +0500

    curd

--- CurdOperation/src/routers/routes.js
+++ CurdOperation/src/routers/routes.js
@@ -2,14 +2,60 @@
 
 const express =require('express')
 const User=require('../models/model')
+const multer=require('multer')
+const path =require('path');
+const { name } = require('ejs');
 // create a express router
 const router=new express.Router();
 // define a express router
 
 //  create a new student
 
-router.get("/",(req,res)=>{
-        res.render('index'); 
+// Uploads Images
+var storage=multer.diskStorage({
+        destination:'public/backend/image',
+        filename:(req,file,cb)=>{
+                console.log(file);
+                cb(null,file.originalname);
+        },
+})
+var upload=multer({
+        storage:storage
+        })
+
+        router.post("/add",upload.single('image'),async(req,res)=>{
+                   
+                // const Insert=async()=>{
+                        try{     
+                        const user=new User({
+                          name:req.body.name,
+                email:req.body.email,
+                Phone:req.body.phone,
+                image:req.file.filename
+                    
+                                })
+                                const result=await User.insertMany([user]);
+                                console.log(result);
+                                }catch(err){
+                                console.log(err);
+                                }
+                //     }
+                //      Insert();
+                 
+
+
+                
+        })
+
+        
+        
+router.get("/",async(req,res)=>{
+       
+        const results =await User.find();
+        res.render('index',{
+                image:'public/backend/image/'+results[8].image});
+        console.log(results[8].image);
+        
 })
 
 
